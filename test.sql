c0b886dea9bf3b9cce7ecd4216a91b0a90553f9a
mjgp2 <matthew.painter@import.io>
2020-11-06 11:34:30 +0000

    Show implicit rollback and double commands

--- test.sql
+++ test.sql
@@ -1,15 +1,25 @@
 CREATE TABLE pgkron.test( id serial primary key, value text not null );
-insert into pgkron.job(sql, run_at, interval) VALUES ( 'INSERT INTO pgkron.test (value) VALUES (now()::text)', now(), '1 millisecond');
+
+insert into pgkron.job(sql, run_at, interval) VALUES ( 'INSERT INTO pgkron.test (value) VALUES (''ok'')', now(), '1 hour');
+insert into pgkron.job(sql, run_at, interval) VALUES ( 'INSERT INTO pgkron.test (value) VALUES (''fail''); bad sql', now(), '1 hour');
 insert into pgkron.job(sql, run_at, interval) VALUES ( 'bad sql', now(), '1 hour');
+
+insert into pgkron.job(sql, run_at, interval) VALUES ($$
+INSERT INTO pgkron.test (value) VALUES ('two-1');
+INSERT INTO pgkron.test (value) VALUES ('two-2');
+$$), now(), '1 hour');
 
 -- run any pending jobs
 CALL pgkron.run();
 
--- should have a couple of results in here now
+-- should have a row for each job
 select * from pgkron.job_log;
+
+-- have the single ok, and the two rows from double
+select * from pgkron.test;
 
 -- this time only the ok sql should run
 CALL pgkron.run();
 
--- we will have 3 results in here now because the bad SQL will not need re-running
 select * from pgkron.job_log;
+select * from pgkron.test;
